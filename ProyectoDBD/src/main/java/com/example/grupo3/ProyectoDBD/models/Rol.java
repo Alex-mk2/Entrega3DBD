cfa04a9f55a32cc3cb2284740b2a394c3193ed2c
HazmatMask <alejandro.rojas.t@usach.cl>
2023-07-29 22:41:46 -0400

    [Alejandro Rojas] - Clase Rol y Sesion creadas y establecidos sus setter/getter

--- ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/models/Rol.java
+++ ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/models/Rol.java
@@ -1,4 +1,47 @@
 package com.example.grupo3.ProyectoDBD.models;
 
 public class Rol {
+    int id_rol;
+    String nombre_rol;
+    String descripcion_rol;
+    String permisos;
+
+    public Rol(int id_rol, String nombre_rol, String descripcion_rol, String permisos) {
+        this.id_rol = id_rol;
+        this.nombre_rol = nombre_rol;
+        this.descripcion_rol = descripcion_rol;
+        this.permisos = permisos;
+    }
+
+    public int getId_rol() {
+        return id_rol;
+    }
+
+    public void setId_rol(int id_rol) {
+        this.id_rol = id_rol;
+    }
+
+    public String getNombre_rol() {
+        return nombre_rol;
+    }
+
+    public void setNombre_rol(String nombre_rol) {
+        this.nombre_rol = nombre_rol;
+    }
+
+    public String getDescripcion_rol() {
+        return descripcion_rol;
+    }
+
+    public void setDescripcion_rol(String descripcion_rol) {
+        this.descripcion_rol = descripcion_rol;
+    }
+
+    public String getPermisos() {
+        return permisos;
+    }
+
+    public void setPermisos(String permisos) {
+        this.permisos = permisos;
+    }
 }
--- ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/models/Sesion.java
+++ ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/models/Sesion.java
@@ -1,4 +1,48 @@
 package com.example.grupo3.ProyectoDBD.models;
+import java.time.LocalDateTime;
 
 public class Sesion {
+    int id_sesion;
+    int id_usuario;
+    LocalDateTime inicio_sesion;
+    LocalDateTime fin_sesion;
+
+    public Sesion(int id_sesion, int id_usuario, LocalDateTime inicio_sesion, LocalDateTime fin_sesion) {
+        this.id_sesion = id_sesion;
+        this.id_usuario = id_usuario;
+        this.inicio_sesion = inicio_sesion;
+        this.fin_sesion = fin_sesion;
+    }
+
+    public int getId_sesion() {
+        return id_sesion;
+    }
+
+    public void setId_sesion(int id_sesion) {
+        this.id_sesion = id_sesion;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public LocalDateTime getInicio_sesion() {
+        return inicio_sesion;
+    }
+
+    public void setInicio_sesion(LocalDateTime inicio_sesion) {
+        this.inicio_sesion = inicio_sesion;
+    }
+
+    public LocalDateTime getFin_sesion() {
+        return fin_sesion;
+    }
+
+    public void setFin_sesion(LocalDateTime fin_sesion) {
+        this.fin_sesion = fin_sesion;
+    }
 }
