1111630fa4b391237cb4c3599ec6decc85bacce9
Jaime Vadell <jaime.vadell@usach.cl>
2023-07-29 22:26:40 -0400

    Clase Libro y metodo de pago

--- ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/models/Libro.java
+++ ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/models/Libro.java
@@ -1,4 +1,97 @@
 package com.example.grupo3.ProyectoDBD.models;
 
 public class Libro {
+    private Integer id_libro;
+
+    private String autor;
+
+    private String titulo;
+
+    private Integer precio;
+
+    private Integer stock;
+
+    private Integer visitas;
+
+    private Integer favoritos;
+
+    private Integer id_usuario;
+
+    public Libro(Integer id_libro, String autor, String titulo, Integer precio, Integer stock, Integer visitas, Integer favoritos, Integer id_usuario) {
+        this.id_libro = id_libro;
+        this.autor = autor;
+        this.titulo = titulo;
+        this.precio = precio;
+        this.stock = stock;
+        this.visitas = visitas;
+        this.favoritos = favoritos;
+        this.id_usuario = id_usuario;
+    }
+
+    public Integer getId_libro() {
+        return id_libro;
+    }
+
+    public void setId_libro(Integer id_libro) {
+        this.id_libro = id_libro;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Integer getVisitas() {
+        return visitas;
+    }
+
+    public void setVisitas(Integer visitas) {
+        this.visitas = visitas;
+    }
+
+    public Integer getFavoritos() {
+        return favoritos;
+    }
+
+    public void setFavoritos(Integer favoritos) {
+        this.favoritos = favoritos;
+    }
+
+    public Integer getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(Integer id_usuario) {
+        this.id_usuario = id_usuario;
+    }
 }
+
+
+
--- ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/models/MetodoPago.java
+++ ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/models/MetodoPago.java
@@ -1,4 +1,27 @@
 package com.example.grupo3.ProyectoDBD.models;
 
-public class MetodoPago {
+public class Metodo_Pago {
+    private Integer id_metodo_pago;
+
+    private String tipo_pago;
+
+    public Metodo_Pago(Integer id_metodo_pago, String tipo_pago) {
+        this.id_metodo_pago = id_metodo_pago;
+        this.tipo_pago = tipo_pago;
+    }
+    public Integer getId_metodo_pago() {
+        return id_metodo_pago;
+    }
+
+    public void setId_metodo_pago(Integer id_metodo_pago) {
+        this.id_metodo_pago = id_metodo_pago;
+    }
+
+    public String getTipo_pago() {
+        return tipo_pago;
+    }
+
+    public void setTipo_pago(String tipo_pago) {
+        this.tipo_pago = tipo_pago;
+    }
 }
