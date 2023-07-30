2f7ae2b2fe0add792da1e792e792998393774b28
Alex <alex.mellado.g@usach.cl>
2023-07-29 22:11:20 -0400

    [Alex Mellado] -Creacion de tablas reseña y restriccion, atributos, constructores, getters y setters

--- ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/models/Reseña.java
+++ ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/models/Reseña.java
@@ -1,4 +1,63 @@
 package com.example.grupo3.ProyectoDBD.models;
 
-public class Reseña {
+import java.util.Date;
+
+public class Reseña{
+    private Integer id_usuario;
+    private Integer id_libro;
+    private Integer nota;
+    private String descripcion;
+    private Date fecha;
+
+    public Reseña(Integer id_usuario, Integer id_libro, Integer nota, String descripcion, Date fecha) {
+        this.id_usuario = id_usuario;
+        this.id_libro = id_libro;
+        this.nota = nota;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+    }
+
+    public Reseña(){
+
+    }
+
+    public Integer getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(Integer id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public Integer getId_libro() {
+        return id_libro;
+    }
+
+    public void setId_libro(Integer id_libro) {
+        this.id_libro = id_libro;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
 }
--- ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/models/Restriccion.java
+++ ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/models/Restriccion.java
@@ -1,4 +1,31 @@
 package com.example.grupo3.ProyectoDBD.models;
 
-public class Restriccion {
+public class Restriccion{
+    private Integer id_restriccion;
+    private Integer edad_minima;
+
+    public Restriccion(Integer id_restriccion, Integer edad_minima) {
+        this.id_restriccion = id_restriccion;
+        this.edad_minima = edad_minima;
+    }
+
+    public Restriccion(){
+
+    }
+
+    public Integer getId_restriccion() {
+        return id_restriccion;
+    }
+
+    public void setId_restriccion(Integer id_restriccion) {
+        this.id_restriccion = id_restriccion;
+    }
+
+    public Integer getEdad_minima() {
+        return edad_minima;
+    }
+
+    public void setEdad_minima(Integer edad_minima) {
+        this.edad_minima = edad_minima;
+    }
 }
