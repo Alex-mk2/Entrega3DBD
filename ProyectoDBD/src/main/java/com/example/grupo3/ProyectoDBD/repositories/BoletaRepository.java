5fd7b0a81f0d8d74acabb936fe5bc3883d0da54d
Ignacio Riquelme <ignacio.riquelme.r@usach.cl>
2023-07-30 19:17:39 -0400

    [Ignacio Riquelme] - Se agrega Service de Boleta

--- ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/repositories/BoletaRepository.java
+++ ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/repositories/BoletaRepository.java
@@ -5,13 +5,14 @@ import com.example.grupo3.ProyectoDBD.models.Boleta;
 import java.util.List;
 
 public interface BoletaRepository {
-    Boleta create(Boleta boleta);
 
-    List<Boleta> getAll();
+    public Boleta create(Boleta boleta);
 
-    List<Boleta> show(Integer id_boleta);
+    public List<Boleta> getAll();
 
-    String update(Boleta boleta, Integer id_boleta);
+    public List<Boleta> show(Integer id_boleta);
 
-    void delete(Integer id_boleta);
+    public String update(Boleta boleta, Integer id_boleta);
+
+    public void delete(Integer id_boleta);
 }
---
+++ ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/services/CategoriaService.java
@@ -0,0 +1,9 @@
+package com.example.grupo3.ProyectoDBD.services;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class CategoriaService {
+}
