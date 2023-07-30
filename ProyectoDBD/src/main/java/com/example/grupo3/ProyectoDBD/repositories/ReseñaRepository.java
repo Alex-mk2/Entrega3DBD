a2710fd3d4d3623fbf2e37250c844d7f85a1269b
Alex <alex.mellado.g@usach.cl>
2023-07-30 00:32:39 -0400

    [Alex Mellado] -Creacion metodos Reseña service y Reseña repository

--- ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/repositories/ReseñaRepository.java
+++ ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/repositories/ReseñaRepository.java
@@ -1,4 +1,31 @@
 package com.example.grupo3.ProyectoDBD.repositories;
+import com.example.grupo3.ProyectoDBD.models.Reseña;
 
 public interface ReseñaRepository {
+    /**
+     * Inserta una reseña en la tabla reseña
+     * al ser atributos obligatorios (menos id), si son nulos se lanzara una excepcion si no encuentra el id
+     * o fallo de la conexion
+     * @param Reseña objeto de la clase Reseña que contiene atributos definidos en la estructura insertar Reseña, provenientes del MR
+     * @return su retorno es una reseña creada, caso contrario nulo si falla la operacion
+     * @throws Exception si uno de los atributos es nulo o esta vacio.
+     *
+     */
+
+    public Reseña create(Reseña Reseña);
+
+    /**
+     * Inserta datos en una clase Reseña en la tabla Reseña.
+     * al ser atributos obligatorios (menos id), si son nulos se lanzara una excepcion si no encuentra el id
+     * o fallo de la conexion
+     *
+     * @param Reseña objeto de la clase Reseña que tiene los atributos correspondientes a insertar en una Reseña.
+     * @param id_usuario string alfanumerico correspondiente al id de la Reseña  a la cual se desea insertar datos.
+     * @return retorna un string que indica el exito de la actualización de los datos del id_usuario.
+     * @throws Exception Si uno de los atributos es null o vacio o si no encuentra el id del usuario.
+     */
+
+    public String update(Reseña Reseña, Integer id_usuario);
+
+
 }
--- ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/services/ReseñaService.java
+++ ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/services/ReseñaService.java
@@ -1,4 +1,27 @@
 package com.example.grupo3.ProyectoDBD.services;
 
-public class ReseñaService {
+import com.example.grupo3.ProyectoDBD.models.Reseña;
+import com.example.grupo3.ProyectoDBD.repositories.ReseñaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class ReseñaService{
+    private final com.example.grupo3.ProyectoDBD.repositories.ReseñaRepository reseñaRepository;
+    ReseñaService(ReseñaRepository reseñaRepository) {
+        this.reseñaRepository = reseñaRepository;
+    }
+
+
+    //Crear Reseña//
+    @PostMapping("/Reseña/")
+    @ResponseBody
+    public Reseña create(Reseña Reseña){
+        Reseña newReseña = reseñaRepository.create(Reseña);
+        return newReseña;
+    }
+
 }
