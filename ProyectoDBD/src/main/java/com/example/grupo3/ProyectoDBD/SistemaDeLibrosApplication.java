ce34d27966656b4bb1ca787515e076bb7b340021
Ignacio Riquelme <ignacio.riquelme.r@usach.cl>
2023-07-29 21:30:26 -0400

    [Ignacio Riquelme] - Se inicia el proyecto

---
+++ ProyectoDBD/src/main/java/com/example/grupo3/ProyectoDBD/SistemaDeLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupo3.ProyectoDBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeLibrosApplication.class, args);
+	}
+
+}
---
+++ ProyectoDBD/src/test/java/com/example/grupo3/ProyectoDBD/SistemaDeLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupo3.ProyectoDBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
